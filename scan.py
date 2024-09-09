4b69dc55d434fc2c7249880cfd37b72435b755c1
Ssing77 <sahajsingh67@gmail.com>
2024-09-08 20:31:41 -0400

    computing ratio of the old height of the photo vs the new.

--- scan.py
+++ scan.py
@@ -1,5 +1,4 @@
 import pyimagesearch
-from pyimagesearch.transform import four_point_transform
 from skimage.filters import threshold_local
 import numpy as np
 import argparse
@@ -9,4 +8,9 @@ ap = argparse.ArgumentParser()
 ap.add_argument("'i", "--image", required = True,
                 help = "Path to the image to be scanned")
 args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
 
